2a9635826ce191702491a634c31a5c41531c2f3c
Deyvison <deyvisonvitor85@gmail.com>
2026-05-22 00:04:08 -0300

    feat: adicionando telas HomePage, ListPage e MapPage no pacote ui

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,20 +1,16 @@
 package com.example.weatherapp
 
-import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.weatherapp.ui.HomePage
 import com.example.weatherapp.ui.theme.WeatherAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,30 +28,6 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
-
-@Composable
-fun HomePage(modifier: Modifier = Modifier) {
-
-    val context = LocalContext.current
-    val activity = context as? Activity
-
-    Scaffold(modifier = modifier.fillMaxSize()) { innerPadding ->
-
-        Column(modifier = Modifier
-            .padding(innerPadding)
-            .fillMaxSize()) {
-            Text(text = "Bem vindo ao Sistema!")
-
-            Button(onClick =  {
-                activity?.finish()
-            }){
-                Text("Sair")
-            }
-        }
-
-    }
-
 }
 
 @Preview(showBackground = true)
