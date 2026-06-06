db29b897aca75f1070a5bbf2f07b45487b8c629e
Deyvison <deyvisonvitor85@gmail.com>
2026-06-05 23:56:26 -0300

    feat: adiciona marcadores e interação no mapa

--- app/src/main/java/com/example/weatherapp/ui/MapPage.kt
+++ app/src/main/java/com/example/weatherapp/ui/MapPage.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
@@ -14,25 +15,87 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
 import com.example.weatherapp.viewmodel.MainViewModel
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
 
 @Composable
 fun MapPage(
     modifier: Modifier = Modifier,
     viewModel: MainViewModel
 ) {
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-            .background(Color.Gray)
-            .wrapContentSize(Alignment.Center)
+    val recife = remember {
+        MarkerState(
+            LatLng(-8.05, -34.9)
+        )
+    }
+
+    val caruaru = remember {
+        MarkerState(
+            LatLng(-8.27, -35.98)
+        )
+    }
+
+    val joaopessoa = remember {
+        MarkerState(
+            LatLng(-7.12, -34.84)
+        )
+    }
+
+    val camPosState = rememberCameraPositionState ()
+
+
+    GoogleMap(
+        modifier = modifier.fillMaxSize(),
+        cameraPositionState = camPosState,
+        onMapClick = {
+            viewModel.add(
+                "Cidade@${it.latitude}:${it.longitude}",
+                location = it
+            )
+        }
     ) {
-        Text(
-            text = "Mapa",
-            fontWeight = FontWeight.Bold,
-            color = Color.White,
-            modifier = Modifier.align(CenterHorizontally),
-            textAlign = TextAlign.Center,
-            fontSize = 20.sp
+
+        viewModel.cities.forEach { city ->
+
+            if (city.location != null) {
+
+                Marker(
+                    state = MarkerState(position = city.location),
+                    title = city.name,
+                    snippet = "${city.location}"
+                )
+            }
+        }
+
+        Marker(
+            state = recife,
+            title = "Recife",
+            snippet = "Marcador em Recife",
+            icon = BitmapDescriptorFactory.defaultMarker(
+                BitmapDescriptorFactory.HUE_BLUE
+            )
+        )
+
+        Marker(
+            state = caruaru,
+            title = "Caruaru",
+            snippet = "Marcador em Caruaru",
+            icon = BitmapDescriptorFactory.defaultMarker(
+                BitmapDescriptorFactory.HUE_RED
+            )
+        )
+
+        Marker(
+            state = joaopessoa,
+            title = "João Pessoa",
+            snippet = "Marcador em João Pessoa",
+            icon = BitmapDescriptorFactory.defaultMarker(
+                BitmapDescriptorFactory.HUE_GREEN
+            )
         )
     }
 }
