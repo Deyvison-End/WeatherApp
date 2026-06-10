bbdda12a21cc8ac32623d782dab81ccb2e1fb877
Deyvison <deyvisonvitor85@gmail.com>
2026-06-10 00:37:11 -0300

    Configura Firebase Firestore para persistência de dados

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
 
     implementation("androidx.navigation:navigation-compose:2.9.8")
     implementation(libs.firebase.auth)
+    implementation(libs.firebase.firestore)
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
     implementation("androidx.compose.material:material-icons-extended")
     // Google maps
