ebc36f0f27380ac5e90ecf3c846bd9f6984b24a7
Deyvison <deyvisonvitor85@gmail.com>
2026-06-28 09:03:29 -0300

    Configura WEATHER_API_KEY no BuildConfig e adiciona Retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
@@ -18,8 +20,17 @@ android {
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        val keyFile = project.rootProject.file("local.properties")
+        val props = Properties()
+        props.load(keyFile.inputStream())
+
+        buildConfigField(
+            "String",
+            "WEATHER_API_KEY",
+            props.getProperty("WEATHER_API_KEY")
+        )
     }
 
     buildTypes {
@@ -39,6 +50,7 @@ android {
 
     buildFeatures {
         compose = true
+        buildConfig = true
     }
 }
 
@@ -56,6 +68,8 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.material3)
+    implementation("com.squareup.retrofit2:retrofit:3.0.0")
+    implementation("com.squareup.retrofit2:converter-gson:3.0.0")
 
     implementation("androidx.navigation:navigation-compose:2.9.8")
     implementation(libs.firebase.auth)
